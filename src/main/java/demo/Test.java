b97c155f719c4582ecb3f3f4734f20787a3cd5b2
RiccardoMoretti <sakmoro@msn.com>
2014-08-08 10:12:37 +0200

    OK, funzionante

--- src/main/java/demo/Test.java
+++ src/main/java/demo/Test.java
@@ -13,84 +13,64 @@ import joiner.commons.twins.TwinFunction;
 import joiner.computational.ComputationalServer;
 import joiner.server.DataServer;
 
-						/* CLASS FOR TESTING */
+/* CLASS FOR TESTING */
 
 public class Test {
-	
+
 	private final static Logger logger = LoggerFactory.getLogger(Test.class);
 
 	private final static int THOUSAND = 1000;
 	private final static int MILLION  = THOUSAND * THOUSAND;
 	private final static int BILLION  = THOUSAND * MILLION;
-	
+
 	private final static int NUMMARKERS  = 100;
 	private final static int ONETWINEVERY  = 10;
-	
+
 	private final static int DOMAINSTARTSAT  = 0;
 	private final static int DOMAINENDSAT  = 50;
-	
-//	private final static int SOGLIA  = 1;
-	
+
 	private final static int TUPLETABLEL  = 10;
 	private final static int TUPLETABLER  = 5;
-	
+
 	private final static int NUMTESTCASE  = 5;
-	
+
 	public static void main(String[] args) throws Exception {
 
-		// create the markers 		
+		// create the markers
 		Set<String> markers = new HashSet<String>();
-		
+
 		for (int i = 0; i < NUMMARKERS; ++i)
 			markers.add(Integer.toString(i));
 
 		// create the twin function ( one every 10 )
 		TwinFunction twin = new HashTwinFunction(ONETWINEVERY);
-				
-		DataServerConnector sc1[] = new DataServerConnector[NUMTESTCASE];
-		DataServerConnector sc2[] = new DataServerConnector[NUMTESTCASE];
-	
-		// create the computational server 
+
+		// create the computational server
 		ComputationalServer cs = new ComputationalServer(5555, 2);
+		cs.last(NUMTESTCASE);
 		cs.start();
 
-		// create the client 
-		Client client[] = new Client[NUMTESTCASE];
-				
-		DataServer ds[] = new DataServer[5];
-
-/*	L'idea è quella di eseguire il tutto NUMTESTCASE volte, ogni volta passando il valore di "i"
- * 		come valor di soglia ( agendo sempre su db con lo stesso numero di tuple ) e vedere
- * 		come cambia la percentuale di tuple spurie nel risultato.
- * 	Se eseguo manualmente il tutto ( eseguo con un valore di soglia, fermo l'esecuzione e la rilancio
- * 		con un altro valore ) funziona, se lo automatizzato ( come di seguito ) non funziona
- * 		ma non viene generato nessun errore quindi non so come comportarmi.
- *  Penso che il tutto sia dovuto alla mancata chiusura di qualche socket ma ho già controllato
- *  	e mi sembra apposto.. non so proprio cosa fare! 
- *  Penso che il problema sia nel client, in quanto alla seconda esecuzione si arriva fino alla "sua parte"
- *  	senza errori e poi si blocca tutto.
- */
-		for ( int i = 1 ; i < NUMTESTCASE ; i++ )
+		for ( int i = 0 ; i < NUMTESTCASE ; i++ )
 		{
-			// create the data server	
-			ds[i] = new DataServer(3000, "ThisIsASecretKey", markers, twin , DOMAINSTARTSAT, DOMAINENDSAT, i);
-			ds[i].start();
-		
-			sc1[i] = new DataServerConnector("tcp://127.0.0.1:3000", "1", Integer.toString(TUPLETABLEL));
-			sc2[i] = new DataServerConnector("tcp://127.0.0.1:3000", "1", Integer.toString (TUPLETABLER));
+			// create the data server
+			DataServer ds = new DataServer(3000, "ThisIsASecretKey", markers, twin , DOMAINSTARTSAT, DOMAINENDSAT, i + 1);
+			ds.last(4);
+			ds.start();
 
-			client[i] = new Client	("ThisIsASecretKey", markers, twin);
-			client[i].connect("tcp://127.0.0.1:5555");
-		
+			DataServerConnector sc1 = new DataServerConnector("tcp://127.0.0.1:3000", "1", Integer.toString(TUPLETABLEL));
+			DataServerConnector sc2 = new DataServerConnector("tcp://127.0.0.1:3000", "1", Integer.toString (TUPLETABLER));
+
+			// create the client
+			Client client = new Client ("ThisIsASecretKey", markers, twin);
+			client.connect("tcp://127.0.0.1:5555");
+
 			long initial = System.nanoTime();
-		
-			client[i].join(sc1[i], sc2[i]);
-		
+			client.join(sc1, sc2);
 			float elapsed = (System.nanoTime() - initial) / ((float) BILLION);
 			logger.info("Elapsed time: {} s", elapsed);
-		
-			client[i].destroy();
-			
-		}	
-	}	
+
+			client.destroy();
+
+		}
+	}
 }
